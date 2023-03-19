51e39789e23b25bafd0f8df655ae58cd836bfbb8
PedroPertusi <65266327+PedroPertusi@users.noreply.github.com>
2023-03-19 18:06:06 -0300

    teste

--- camera.py
+++ camera.py
@@ -13,8 +13,8 @@ def criar_indices(min_i, max_i, min_j, max_j):
 def run():
     cap = cv.VideoCapture(0)
 
-    width = 320
-    height = 240
+    width = 300
+    height = 300
     ang = 0
 
     if not cap.isOpened():
@@ -36,8 +36,11 @@ def run():
         X = criar_indices(0,width,0,height)
         X = np.vstack((X,np.ones(X.shape[1])))
 
-        T = np.array([[1, 0, -height/2], [0, 1, -width/2], [0, 0,1]])
-        T2 = np.array([[1, 0, height/2], [0, 1, width/2], [0, 0,1]])
+        # T = np.array([[1, 0, -height/2], [0, 1, -width/2], [0, 0,1]])
+        # T2 = np.array([[1, 0, height/2], [0, 1, width/2], [0, 0,1]])
+        T = np.array([[1, 0, -150], [0, 1, -150], [0, 0,1]])
+        T2 = np.array([[1, 0, 150], [0, 1, 150], [0, 0,1]])
+
         R = np.array([[np.cos(math.radians(ang)), -np.sin(math.radians(ang)), 0], [np.sin(math.radians(ang)), np.cos(math.radians(ang)), 0], [0, 0, 1]])
         Xd = T2 @ R @ T @ X
 
@@ -49,9 +52,10 @@ def run():
 
         Xd, X = Xd[:,filter], X[:,filter]
 
-        fx = image.shape[0]-1
-        fy = image.shape[1]-1
-        image_[Xd[0,:], Xd[1,:], :] = image[X[0,fx], X[1,fy], :]
+        fx = image.shape[0]
+        fy = image.shape[1]
+
+        image_[Xd[0,:], Xd[1,:], :] = image[X[0,:]-fx, X[1,:]-fy, :]
         ang += 1
 
         cv.imshow('Minha Imagem!', image_)
