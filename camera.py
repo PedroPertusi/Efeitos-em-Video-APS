5785b352206c6576255097c28a7e29d7d4b0a7fc
PedroPertusi <65266327+PedroPertusi@users.noreply.github.com>
2023-03-19 17:21:01 -0300

    teste

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import itertools
 import cv2 as cv
 
@@ -30,12 +31,22 @@ def run():
         frame = cv.resize(frame, (width,height), interpolation =cv.INTER_AREA)
         image = np.array(frame).astype(float)/255
 
-        image_ = np.zeros_like(image.shape)
+        image_ = np.zeros_like(image)
+
         X = criar_indices(0,width,0,height)
-        rotate = np.array([[np.cos(ang), -np.sin(ang), 0], [np.sin(ang), np.cos(ang), 0], [0, 0, 1]])
-        Xd = rotate @ X
-        filter = (Xd[0,:] >= 0) & (Xd[0,:] <= image_.shape[0]) & (Xd[1,:] >= 0) & (Xd[1,:] <= image_.shape[1])
+        X = np.vstack((X,np.ones(X.shape[1])))
+
+        R = np.array([[np.cos(math.radians(ang)), -np.sin(math.radians(ang)), 0], [np.sin(math.radians(ang)), np.cos(math.radians(ang)), 0], [0, 0, 1]])
+        Xd = R @ X
+
+        Xd = Xd.astype(int)
+        X = X.astype(int)
+
+        filter = (Xd[0,:] >= 0) & (Xd[0,:] <= image.shape[0]) & (Xd[1,:] >= 0) & (Xd[1,:] <= image.shape[1])
+
         Xd, X = Xd[:,filter], X[:,filter]
+
+        print(Xd.shape,X.shape)
         image_[Xd[0,:], Xd[1,:], :] = image[X[0,:], X[1,:], :]
         ang += 1
 
@@ -48,3 +59,21 @@ def run():
     cv.destroyAllWindows()
 
 run()
+
+
+"""
+assigns values from the image array to the image_ array using the pixel coordinates specified in the X and Xd arrays.
+
+Xd[0,:] and Xd[1,:] are two arrays of equal length that contain the x and y coordinates, respectively, of the transformed pixels. 
+
+X[0,:] and X[1,:] are also two arrays of equal length that contain the x and y coordinates, respectively, of the original pixels.
+
+The : at the end of each index means that we are selecting all values along the third axis, which represents the color channels of the image.
+
+Thus, the line of code assigns to each pixel of the image_ array at the transformed pixel coordinates specified by Xd[0,:] and Xd[1,:] the color values 
+
+from the corresponding pixel coordinates in the original image array specified by X[0,:] and X[1,:].
+
+In other words, this line of code copies a rectangular region from the original image to the transformed image, and it's the key step for the image rotation effect in the code.
+
+"""
